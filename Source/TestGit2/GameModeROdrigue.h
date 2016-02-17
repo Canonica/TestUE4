ac99d8a581aeff9c1c1104a0159166e2bb3eae74
Hansotte Joachim <j.hansotte@rubika-edu.com>
2016-02-17 22:05:20 +0100

    Commmmiiiiiiittttt RORORORORORO

---
+++ Source/TestGit2/GameModeROdrigue.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GameModeROdrigue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTGIT2_API AGameModeROdrigue : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
